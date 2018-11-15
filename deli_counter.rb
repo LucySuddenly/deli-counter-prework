822295322fe6f3e1a7938829d59e3c08fa88f96a
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-15 17:18:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,10 @@ def line(array)
     puts "The line is currently empty."
   else
     line = []
+    counter = 1
     array.each do |item|
-      line << item
+      line << "#{counter}. #{item}"
     end 
+    puts "The line is currently: #{line.join}"
   end 
 end 
